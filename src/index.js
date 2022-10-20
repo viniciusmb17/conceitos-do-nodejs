e840c1e0bb6a4bda9993a4ae83b4a3fc2525b95b
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-10-20 10:25:30 -0300

    feat: create user todo

--- src/index.js
+++ src/index.js
@@ -47,11 +47,26 @@ app.post('/users', (request, response) => {
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const { user } = request
 
-  return response.json(user.todos)
+  return response.status(200).json(user.todos)
 })
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+  const { title, deadline } = request.body
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date() 
+  }
+
+  user.todos.push(todo)
+
+  return response.status(201).json(todo)
+
+
 })
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
